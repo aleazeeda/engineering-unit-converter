4cad3ea383cd24fb5f10dfc75c39870bf14b0815
aleazeeda <78604457+aleazeeda@users.noreply.github.com>
2026-01-13 23:09:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,49 @@
 function hideAll() {
-  document.getElementById("frontPage").classList.add("hidden");
-  document.getElementById("menuPage").classList.add("hidden");
-  document.getElementById("frequentPage").classList.add("hidden");
-  document.getElementById("converterPage").classList.add("hidden");
-  document.getElementById("endPage").classList.add("hidden");
+  for (let i = 1; i <= 5; i++) {
+    document.getElementById("page" + i).classList.add("hidden");
+  }
 }
 
-function goToMenu() {
+function goPage1() {
   hideAll();
-  document.getElementById("menuPage").classList.remove("hidden");
+  document.getElementById("page1").classList.remove("hidden");
 }
 
-function goToConverter() {
+function goPage2() {
   hideAll();
-  document.getElementById("converterPage").classList.remove("hidden");
+  document.getElementById("page2").classList.remove("hidden");
 }
 
-function showFrequent() {
+function goPage3() {
   hideAll();
-  document.getElementById("frequentPage").classList.remove("hidden");
+  document.getElementById("page3").classList.remove("hidden");
 }
 
-function goBack() {
+function goPage4() {
   hideAll();
-  document.getElementById("frontPage").classList.remove("hidden");
+  document.getElementById("page4").classList.remove("hidden");
 }
 
-function goToEnd() {
+function goEnd() {
   hideAll();
-  document.getElementById("endPage").classList.remove("hidden");
-}
-
-function restart() {
-  hideAll();
-  document.getElementById("frontPage").classList.remove("hidden");
+  document.getElementById("page5").classList.remove("hidden");
 }
 
 function convert() {
   let value = document.getElementById("value").value;
-  let unit = document.getElementById("unit").value;
+  let type = document.getElementById("conversion").value;
   let result = 0;
 
-  if (unit === "cm-m") {
-    result = value / 100;
-  } else if (unit === "km-m") {
-    result = value * 1000;
-  } else if (unit === "cm-km") {
+  if (type === "cm-km") {
     result = value / 100000;
+  } else if (type === "km-m") {
+    result = value * 1000;
+  } else if (type === "c-k") {
+    result = Number(value) + 273.15;
+  } else if (type === "cm-mm") {
+    result = value * 10;
+  } else if (type === "m-cm") {
+    result = value * 100;
   }
 
   document.getElementById("result").innerText = result;
